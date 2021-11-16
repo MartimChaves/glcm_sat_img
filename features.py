5e3e1aae7e91a0062b098376ff254011e616b897
MartimChaves <mgrc99@gmail.com>
2021-11-16 15:24:36 +0000

    Extract and select features

---
+++ features.py
@@ -0,0 +1,27 @@
+# imports
+
+### exploratory data analysis
+
+# random images
+
+# average image for each class
+# ravel image into vector
+# reshape to original size
+
+# difference between average of images
+
+# variability 
+
+# eigenfaces
+
+# RGB/HSV/Grey
+# Max value, min value, mean, std for each class
+# correlation for each class and in general
+
+
+
+### train val test split
+
+### feature development
+
+### feature selection
