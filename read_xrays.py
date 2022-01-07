90e6fcddd6c7a8de4b80ace5715773f0b699b10b
MartimChaves <mgrc99@gmail.com>
2022-01-07 17:52:25 +0000

    Selecting class samples in TFRec

--- read_xrays.py
+++ read_xrays.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import numpy as np
+import time
 from matplotlib import pyplot as plt
 import os
 
@@ -39,21 +40,34 @@ def image_decoder(data):
     return image, labels
 
 # 3. create dataset
+bs = 128
 datadir = "./data/chest_xrays/archive/data/"
 data_list = [os.path.join(datadir,x) for x in os.listdir(datadir)]
 dataset = tf.data.TFRecordDataset(data_list)
 dataset = dataset.map(image_decoder, num_parallel_calls=tf.data.experimental.AUTOTUNE)
-dataset = dataset.batch(128).prefetch(tf.data.experimental.AUTOTUNE)
+dataset = dataset.batch(bs).prefetch(tf.data.experimental.AUTOTUNE)
 
 """
 num_imgs = 0
 for element in dataset.as_numpy_iterator(): #112120 total imgs :o
     num_imgs += element[0].shape[0]
 """
+
+labels = np.zeros((112120,15))
+
+for idx, element in enumerate(dataset.as_numpy_iterator()): #112120 total imgs :o
+    labels[idx*bs:(idx+1)*bs] = element[1]
+
+class_lbl = np.zeros(15)
+class_lbl[0] = 1
+class_ind = np.where(labels == class_lbl)[0]
+
+
 # save labels
 # use labels to read specific imgs
-arr = np.array([3, 4, 5])
-my_table = {3: True, 4: True, 5: True}
+t = time.process_time()
+arr = class_ind #np.array([3, 4, 5])
+my_table = {x:True for x in class_ind} #{3: True, 4: True, 5: True}
 
 dataset = dataset.enumerate()
 keys_tensor = tf.constant(arr)
@@ -71,5 +85,6 @@ def hash_table_filter(index, value):
 filtered_ds = dataset.filter(hash_table_filter)
 
 final_ds = filtered_ds.map(lambda idx,value: value)
+elapsed_time = time.process_time() - t
 
-print("HEY-O")
+print(elapsed_time)
